8800e28dffafbb5f68c870eb569e1bf9565d8536
judith H <judithhouinsou0@gmail.com>
2025-04-03 17:49:10 +0100

    authenticationis ok!

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,8 +1,13 @@
  import User from '#models/user'
-import { registerUserValidator } from '#validators/auth'
+import { loginUserValidator, registerUserValidator } from '#validators/auth'
 import { cuid } from '@adonisjs/core/helpers'
 import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
+import { dd } from '@adonisjs/core/services/dumper'
+import { writeFile } from 'fs/promises'
+
+import * as jdenticon from 'jdenticon'
+
 
 export default class AuthController {
   register({view}: HttpContext) {
@@ -10,18 +15,41 @@ export default class AuthController {
   }
 
   async handleRegister({request, session,response}: HttpContext){
+    console.log('before vali')
+    // dd(request.all())
       const {email, password, thumbnail, username} = await request.validateUsing(registerUserValidator)
-      if(!thumbnail){}
+
+      if(!thumbnail){
+        const png= jdenticon.toPng(username, 100)
+        await writeFile(`public/users/${username}.png`,png,)
+      }
       else{
-        await thumbnail.move(app.makePath("public/users"), {name : '${cuid()}.$thumbnail.extname'})
+        await thumbnail.move(app.makePath("public/users"), {name :`${cuid()}.$thumbnail.extname`})
 
       }
-      const filePath = 'users/${thumbnail?.fileName}'
-      await User.create({email, username,thumbnail: filePath, password})
-      session.flash("Success","Inscription ok !!!")
-      return response.redirect().toRoute("home")
+      console.log('after val')
+      const filePath =  `users/${thumbnail?.fileName || username + ".png"}`
+      const res = await User.create({email, username,thumbnail: filePath, password})
+      console.log(res)
+      session.flash("success","Inscription ok !!!")
+      return response.redirect().toRoute("auth.login")
   }
   login ({view}: HttpContext) {
     return view.render('pages/auth/login')
+  }
+
+  async handleLogin({request,auth,response,session}:HttpContext){
+    const {email, password} = await request.validateUsing(loginUserValidator)
+    const user = await User.verifyCredentials(email, password)
+    await auth.use("web").login(user)
+    session.flash("success","Connexion ok !!!")
+    return response.redirect().toRoute("home")
+  }
+
+  async logout({auth,session,response,}: HttpContext)
+  {
+    auth.use("web").logout()
+    session.flash("success","Déconnexion ok !!!")
+    return response.redirect().toRoute("auth.login")
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -14,4 +14,7 @@ router.on('/').render('pages/home').as("home")
 router.get('/register',[AuthController,'register']).as("auth.register")
 router.post('/register',[AuthController,'handleRegister'])
 router.get('/login',[AuthController,'login']).as("auth.login")
+router.post('/login',[AuthController,'handleLogin'])
+
+router.delete('/logout',[AuthController,'logout']).as("auth.logout")
 
