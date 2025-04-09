5669d23870254b7a06e8effc3b5e83867858b5fc
judith H <judithhouinsou0@gmail.com>
2025-04-09 14:44:59 +0100

    add ally oauth

--- start/routes.ts
+++ start/routes.ts
@@ -9,12 +9,18 @@
 
 import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+import SocialController from '#controllers/social_controller'
 
 router.on('/').render('pages/home').as("home")
-router.get('/register',[AuthController,'register']).as("auth.register")
-router.post('/register',[AuthController,'handleRegister'])
-router.get('/login',[AuthController,'login']).as("auth.login")
-router.post('/login',[AuthController,'handleLogin'])
+router.get('/register',[AuthController,'register']).as("auth.register").use(middleware.guest())
+router.post('/register',[AuthController,'handleRegister']).use(middleware.guest())
+router.get('/login',[AuthController,'login']).as("auth.login").use(middleware.guest())
+router.post('/login',[AuthController,'handleLogin']).use(middleware.guest())
 
-router.delete('/logout',[AuthController,'logout']).as("auth.logout")
+router.get('/github/redirect',[SocialController,'githubRedirect']).use(middleware.guest()).as("github.redirect")
+
+router.get('/github/callback',[SocialController,'githubCallBack']).use(middleware.guest()).as("github.callback")
+
+router.delete('/logout',[AuthController,'logout']).as("auth.logout").use(middleware.auth())
 
